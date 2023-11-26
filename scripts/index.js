3e429fc6bfcc3a216d42ee5deb9834481632ba5c
Vyacheslav Bogdanov <slavabogdn@gmail.com>
2023-11-26 13:01:06 +0700

    feat: cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template');
 // @todo: DOM узлы
-
+const placesList = document.querySelector('.places__list');
 // @todo: Функция создания карточки
+const addCard = (cardData, deleteCB) => {
+  const card = cardTemplate.content.cloneNode(true);
+  const cardImage = card.querySelector('.card__image');
+  const cardTitle = card.querySelector('.card__title');
+  const cardDeleteBtn = card.querySelector('.card__delete-button');
+  const titleContent = document.createTextNode(cardData.name);
+  cardImage.setAttribute('src', cardData.link);
+  cardImage.setAttribute('alt', `изображение ${cardData.name}`);
+  cardTitle.append(titleContent);
+  cardDeleteBtn.addEventListener('click', deleteCB);
 
+  return card;
+};
 // @todo: Функция удаления карточки
-
+const deleteCard = (e) => {
+  const deleteBtn = e.target;
+  const deleteBtnCardParent = deleteBtn.closest('.card');
+  deleteBtnCardParent.remove();
+};
 // @todo: Вывести карточки на страницу
+initialCards.forEach((card) => {
+  placesList.append(addCard(card, deleteCard));
+});
