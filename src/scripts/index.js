33fa51a377ee74b740c8d95b642d1d46bbd30e0e
Vyacheslav Bogdanov <slavabogdn@gmail.com>
2023-12-05 02:41:58 +0700

    feat: form manipulation

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,7 +8,41 @@ import { closeModal, handleOverLayClose, openModal } from './modal';
 const placesList = document.querySelector('.places__list');
 const editProfileButton = document.querySelector('.profile__edit-button');
 const editPopup = document.querySelector('.popup_type_edit');
-const addNewCardButton = document.querySelector('profile__add-button');
+const addNewCardButton = document.querySelector('.profile__add-button');
+const addNewCardPopup = document.querySelector('.popup_type_new-card');
+
+const avatarFormElement = document.forms['edit-profile'];
+const nameInput = avatarFormElement.name;
+const jobInput = avatarFormElement.description;
+const profileName = document.querySelector('.profile__title');
+const profileDescription = document.querySelector('.profile__description');
+
+const newPlaceForm = document.forms['new-place'];
+const newPlaceName = newPlaceForm['place-name'];
+const newPlaceLink = newPlaceForm.link;
+
+const handleFillAvatarFormInputs = () => {
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileDescription.textContent;
+};
+
+const handleAvatarFormSubmit = (evt) => {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+  avatarFormElement.reset();
+  closeModal(editPopup);
+};
+
+const handleCardFormSubmit = (evt) => {
+  evt.preventDefault();
+  const name = newPlaceName.value;
+  const link = newPlaceLink.value;
+  const newCard = createCard({ name, link }, deleteCard);
+  placesList.prepend(newCard);
+  newPlaceForm.reset();
+  closeModal(addNewCardPopup);
+};
 
 loadImages();
 
@@ -17,14 +51,25 @@ initialCards.forEach((card) => {
   placesList.append(createCard(card, deleteCard));
 });
 
-editProfileButton.addEventListener('click', (evt) => {
-  openModal(editPopup);
-});
-
-editPopup.addEventListener('click', handleOverLayClose);
-
 document.addEventListener('click', (evt) => {
   if (evt.target.classList.contains('popup__close')) {
     closeModal(evt.target.parentNode.parentNode);
   }
 });
+
+editProfileButton.addEventListener('click', () => {
+  openModal(editPopup);
+  handleFillAvatarFormInputs();
+});
+
+editPopup.addEventListener('click', handleOverLayClose);
+
+addNewCardButton.addEventListener('click', () => {
+  openModal(addNewCardPopup);
+});
+
+addNewCardPopup.addEventListener('click', handleOverLayClose);
+
+avatarFormElement.addEventListener('submit', handleAvatarFormSubmit);
+
+newPlaceForm.addEventListener('submit', handleCardFormSubmit);
